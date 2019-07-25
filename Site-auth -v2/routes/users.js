114b8da7c4d60debe2cd006332d87a0b16d5a9fe
Dima Chekurkov <Chekurkov@gmail.com>
2019-07-25 20:15:07 +0200

    Created simple register. then(passport js and bcsrypt password)

--- Site-auth -v2/routes/users.js
+++ Site-auth -v2/routes/users.js
@@ -1,71 +1,52 @@
-// var express = require('express');
-// var router = express.Router();
-// var Joi = require('joi');
-// const passport = require('passport');
+var express = require('express');
+var router = express.Router();
 
-// const User = require('../models/user');
+const User = require('../models/user');
 
-// /* GET users listing. */
-// // router.get('/', function(req, res, next) {
-// //   res.send('respond with a resource');
-// // });
+router.get('/', (req, res) => {
+    res.render('login', {
+        title: 'Login'
+    })
+});
 
-// const userSchema = Joi.object().keys({
-//   email: Joi.string().required(),
-//   username: Joi.string().required(),
-//   password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required(),
-//   confirmationPassword: Joi.any().valid(Joi.ref('password')).required()
-// })
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Register'
+    })
+});
 
-// router.route('/register')
-// .get((req, res) => {
-//   res.render('register');
-// })
-// .post(async (req, res, next) => {
-//   try {
-//     const result = Joi.validate(req.body, userSchema)
-//     if(result.error){
-//       req.flash('error', 'Data entered is not valid. Please try again.');
-//       res.redirect('/users/register');
-//       return;
-//     }
-//     const user = await User.findOne({'email': result.value.email})
-//     if(user){
-//       req.flash('error', 'Email is alreader in use.')
-//       res.redirect('/users/register');
-//       return;
-//     }
-//     const hash = await User.hashPassword(result.value.password);
+router.post('/register', async (req, res) => {
 
-//     delete result.value.confirmationPassword
-//     result.value.password = hash
+    const UserSchema = require('../models/user');
 
-//     const newUser = await new User(result.value)
-//     await newUser.save()
+    let login = req.body.login;
+    let password = req.body.password;
+    let repassword = req.body.repassword;
+    let email = req.body.email;
 
-//     req.flash('success', 'Registration succesfully');
-//     res.redirect('/users/login');
-//   } 
-//   catch (error) {
-//     next(error);
-//   }
-// });
+    if(password === repassword){
+        const CreateUser = new UserSchema({
+            login: login,
+            password: password,
+            email: email
+        });
+        CreateUser.save((err) => {
+            if(err) {
+                console.log(err);
+            }
+            else{
+                res.redirect('/users');
+                console.log("Saved");
+            }
+        })
+    }
+    else{
+        res.render('register', {
+            errors: "Something Wrong"
+        });
+    }
 
-// router.get('/login', (req, res) => {
-//   res.render('login');
-// });
+});
 
-// router.post('/login', (req, res) => {
-//   let email = req.body.email;
-//   let password = req.body.password;
 
-//   User.findOne({email: email})
-//   .then((result) => {
-//     if(password = result.password){
-//       req.flash('success', 'Enter success');
-//       res.redirect('/');
-//     }
-//   })
-// })
-
-// module.exports = router;
+module.exports = router;
