393ac7f9d7c89f262393277ebd8ae023645b1d23
Dima Chekurkov <Chekurkov@gmail.com>
2019-08-12 20:22:40 +0200

    Create joi valid

--- Passport/controllers/register.controller.js
+++ Passport/controllers/register.controller.js
@@ -1,13 +1,49 @@
+const joi = require('joi');
+
 exports.register_page_get = (req, res) => {
     res.render('register', {
         title: 'Register'
     });
 };
 
-
 exports.validation = (req, res, next) => {
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
 
-    next();
+    const Schema = joi.object().keys({
+        email: joi.string().email(),
+        username: joi.string().alphanum().min(3).max(30).required(),
+        password: joi.string().regex(/^[a-zA-Z0-9]{3,30}$/)
+    });
+
+    const resultValidate = Schema.validate({
+        email: email,
+        username: username,
+        password: password
+    }, (err, result) => {
+        if(err){
+           res.render('register', {
+               title: 'Register',
+               message: err.details[0].message
+           });
+        }
+        else{
+            console.log(`Joi valid is success`);
+            next();
+        }
+    });
+}
+
+exports.checkDB = (req, res, next) => {
+    console.log('We are on check DB');
+    // check DB
+    // if okay nextr and deside with messages
+    //next();
+    // res.render('register', {
+    //     title: 'Register',
+    //     linkRedirect: 'http://localhost:3000/signIn'
+    // });
 }
 
 exports.register_page_post = (req, res) => {
--- Passport/routes/register.js
+++ Passport/routes/register.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const register_controller = require('../controllers/register.controller');
 
 router.get('/', register_controller.register_page_get);
-router.post('/', register_controller.validation, register_controller.register_page_post);
+router.post('/', register_controller.validation, register_controller.checkDB, register_controller.register_page_post);
 
 module.exports = router;
 
